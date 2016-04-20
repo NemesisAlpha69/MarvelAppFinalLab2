603a92c76a80be1ac0b27cedfaa5b16eaee3d245
NemesisAlpha69 <isaik@galileo.edu>
2016-04-19 21:33:25 -0600

    Initial commit

---
+++ app/src/main/java/com/android/studyjam/asynctask/fragments/HistoryListFragmentListener.java
@@ -0,0 +1,7 @@
+package com.android.studyjam.asynctask.fragments;
+
+/**
+ * Created by RaxisionE on 4/19/2016.
+ */
+public interface HistoryListFragmentListener {
+}
---
+++ app/src/main/java/com/android/studyjam/asynctask/model/CartoonCharacter.java
@@ -0,0 +1,48 @@
+package com.android.studyjam.asynctask.model;
+
+        import com.google.gson.annotations.SerializedName;
+
+public class CartoonCharacter
+{
+
+    public String objectId;//ObjectId
+
+    @SerializedName("name")
+    public String name;
+    public CharacterImage image;
+
+    public CartoonCharacter()
+    {
+
+    }
+
+    public String getObjectId()
+    {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId)
+    {
+        this.objectId = objectId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public CharacterImage getImage()
+    {
+        return image;
+    }
+
+    public void setImage(CharacterImage image)
+    {
+        this.image = image;
+    }
+}
